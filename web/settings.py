bab0c6fb765e71ec84236fe8b2320b299ecad50a
Brian Otieno <bryomajor@gmail.com>
2019-11-23 22:57:16 +0300

    install psycopg2

--- web/settings.py
+++ web/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'awards',
         'USER': 'moringa',
         'PASSWORD': 'p@$$w0rd',
