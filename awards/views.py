8ec306e11a3689aeb4df3ccfe47ca07d65d1e3ea
Brian Otieno <bryomajor@gmail.com>
2019-11-23 23:16:51 +0300

    create the index view fuction which will render the index page

--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from .models import Profile, Projects, Rates
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import ProfileEditForm, ProjectUploadForm, VotesForm
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Blah!")
+    projects = Projects.objects.all()
+    context = {
+        'projects': projects,
+    }
+    return render(request, 'index.html', context)
