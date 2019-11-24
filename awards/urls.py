6974f05deef7f905ea8e6494e334b7e8be3fc864
Brian Otieno <bryomajor@gmail.com>
2019-11-24 20:34:30 +0300

    add urlconf  for the project page

--- awards/urls.py
+++ awards/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'home'),
+    url(r'^projects/(\d+)', views.projects, name='projects'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
