87e6caf814c082b551b58d7053880a35bdc83fb2
Brian Otieno <bryomajor@gmail.com>
2019-11-24 22:04:22 +0300

    define url patterns for the upload and the search results pages

--- awards/urls.py
+++ awards/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index, name = 'home'),
     url(r'^projects/(\d+)', views.projects, name='projects'),
-    url(r'^profile/(?P<username>\w+)', views.profile, name='profile')
+    url(r'^profile/(?P<username>\w+)', views.profile, name='profile'),
+    url(r'^uploads/', views.post_site, name='post_site'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
 
 if settings.DEBUG:
