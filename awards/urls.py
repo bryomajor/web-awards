f117ad451669eaff4e19184b8d3495da945dc3d2
Brian Otieno <bryomajor@gmail.com>
2019-11-24 21:05:28 +0300

    add a urlconf for the profile

--- awards/urls.py
+++ awards/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name = 'home'),
     url(r'^projects/(\d+)', views.projects, name='projects'),
+    url(r'^profile/(?P<username>\w+)', views.profile, name='profile')
 ]
 
 if settings.DEBUG:
