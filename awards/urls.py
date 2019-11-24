96a0cc48abebb34117033e923473147818a8ffb3
Brian Otieno <bryomajor@gmail.com>
2019-11-24 22:26:41 +0300

    define url pattern for the edit profile view

--- awards/urls.py
+++ awards/urls.py
@@ -6,8 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index, name = 'home'),
     url(r'^projects/(\d+)', views.projects, name='projects'),
-    url(r'^profile/(?P<username>\w+)', views.profile, name='profile'),
+    url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
     url(r'^uploads/', views.post_site, name='post_site'),
+    url(r'^accounts/edit/', views.edit_profile, name='edit_profile'),
     url(r'^search/', views.search_results, name='search_results')
 ]
 
