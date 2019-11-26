d1b4a2b7e68c9320963942d099ff1e97cdd25dd0
Brian Otieno <bryomajor@gmail.com>
2019-11-26 22:46:07 +0300

    add api urlconf

--- awards/urls.py
+++ awards/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
     url(r'^uploads/', views.post_site, name='post_site'),
     url(r'^accounts/edit/', views.edit_profile, name='edit_profile'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^api/profiles/$', views.ProfileList.as_view(),name='profile_list'),
+    url(r'^api/projects/$', views.ProjectsList.as_view(),name='projects_list'),
 ]
 
 if settings.DEBUG:
